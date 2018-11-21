07ca3f137e4d13fc2f1fded0ca0d206c90989600
cchaubey <cchaubey@USBLRCCHAUBEY1.us.deloitte.com>
2018-11-21 18:39:15 +0530

    changed repo calls

--- src/main/java/com/universe/laluna/repository/CustomerRepo.java
+++ src/main/java/com/universe/laluna/repository/CustomerRepo.java
@@ -3,6 +3,7 @@ package com.universe.laluna.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.universe.laluna.dto.CustomerDto;
@@ -10,7 +11,8 @@ import com.universe.laluna.entity.Customer;
 
 @Repository
 public interface CustomerRepo extends JpaRepository<Customer, Long>{
-
-	List<Long> findCustomerId();
 	
+	@Query("Select customerId from Customer")
+	List<Long> getAllCustomerIds();
+
 }
--- src/main/java/com/universe/laluna/service/CustomerService.java
+++ src/main/java/com/universe/laluna/service/CustomerService.java
@@ -64,7 +64,7 @@ public class CustomerService {
 	}
 
 	public List<Long> getAllCustomerIds() {
-		return customerRepo.findCustomerId();
+		return customerRepo.getAllCustomerIds();
 	}
 	
 
